cc1776627cb49f7f594522642939ca60d2bd9a57
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-02-23 15:01:46 -0300

    feat: type str = strings

---
+++ aula03.py
@@ -0,0 +1,21 @@
+"""
+Tipos String em Python
+tipagem dinâmica / forte
+str -> string -> texto
+Strings são textos ques estão dentro de aspas
+"""
+
+# Tipagem dinâmicas
+print(1234)
+
+# Aspas simples
+print('w2k')
+
+# Aspas duplas
+print("w2k")
+
+# Escape
+print("\'w2k\'")
+
+print('"w2k"')
+print("'w2k'")
