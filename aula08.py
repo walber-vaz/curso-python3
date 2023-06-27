5bf86a73148f40fb205b7d20b038395e65ef277e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-27 19:55:43 -0300

    feat: operador aritimetico

---
+++ aula08.py
@@ -0,0 +1,39 @@
+# Operação aritmética:
+# soma,
+# subtração,
+# multiplicação
+# divisão
+# potência
+# divisão inteira
+# resto da divisão
+
+# Soma
+sum = 2 + 2
+print(f"2 + 2 = {sum}")
+
+# Subtração
+sub = 2 - 2
+print(f"2 - 2 = {sub}")
+
+# Multiplicação
+mult = 2 * 2
+print(f"2 * 2 = {mult}")
+
+# Divisão
+# O resultado de uma divisão é sempre um float
+div = 2 / 2
+print(f"2 / 2 = {div}")
+
+# Potência
+pot = 2**2
+print(f"2 ** 2 = {pot}")
+
+# Divisão inteira
+# O resultado de uma divisão inteira é sempre um int
+# se o resultado for float, o python trunca o valor
+div_int = 2 // 2
+print(f"2 // 2 = {div_int}")
+
+# Resto da divisão
+resto = 2 % 2
+print(f"2 % 2 = {resto}")
