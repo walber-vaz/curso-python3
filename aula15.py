0824d8a69208510c27a6d7625400c455397a3829
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-29 17:00:21 -0300

    feat: operation logical (or)

--- aula15.py
+++ aula15.py
@@ -11,3 +11,14 @@ if numero1 > 1 and numero2 > 1:
     print("Os dois números são maiores que 1.")
 else:
     print("Pelo menos um dos números é menor que 1.")
+
+
+# Exemplo 2 or
+# or só é falso se as duas condições forem falsas
+numero1 = 5
+numero2 = 3
+
+if numero1 > 1 or numero2 > 1:
+    print("Pelo menos um dos números é maior que 1.")
+else:
+    print("Os dois números são menores que 1.")
