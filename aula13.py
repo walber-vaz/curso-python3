ec2e1d7d093f9101954108cf02afb8e24937bb09
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-29 14:28:21 -0300

    feat: if/else in python

---
+++ aula13.py
@@ -0,0 +1,9 @@
+# if else
+entrada = input("Quer continuar? [S/N] ").upper()
+
+if entrada == "S":
+    print("Continuando...")
+elif entrada == "N":
+    print("Saindo...")
+else:
+    print("Opção inválida!")
