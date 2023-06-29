a3e16f9f9bb2c39968304c76c8f21260986aeff5
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-29 17:13:26 -0300

    feat: operation logical (in, not in)

---
+++ aula16.py
@@ -0,0 +1,19 @@
+# Operador in e not in
+
+# Operador in
+# Verifica se um elemento está contido em uma lista
+# Retorna True ou False
+nome = 'Python'
+
+if 'P' in nome:
+    print('Existe a letra P em Python')
+else:
+    print('Não existe a letra P em Python')
+
+# Operador not in
+# Verifica se um elemento não está contido em uma lista
+# Retorna True ou False
+if 'P' not in nome:
+    print('Existe a letra P em Python')
+else:
+    print('Não existe a letra P em Python')
