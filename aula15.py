9c548c29c0b1fda460d2e63c6f659e4c7d2653ba
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-29 15:15:26 -0300

    feat: operation logical (and)

---
+++ aula15.py
@@ -0,0 +1,13 @@
+# Operador lógico: and, or, not
+# and: verdadeiro se as duas condições forem verdadeiras
+# or: verdadeiro se uma das condições for verdadeira
+# not: inverte o valor lógico
+
+# Exemplo 1:
+numero1 = 2
+numero2 = 3
+
+if numero1 > 1 and numero2 > 1:
+    print("Os dois números são maiores que 1.")
+else:
+    print("Pelo menos um dos números é menor que 1.")
