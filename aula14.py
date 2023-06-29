67df47ff1507cc74857a5871ab5a8d79bb4c581b
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-29 15:07:08 -0300

    feat: operation reletion

---
+++ aula14.py
@@ -0,0 +1,27 @@
+# Operadores relacionais
+"""
+== igual
+> maior
+>= maior ou igual
+< menor
+<= menor ou igual
+!= diferente
+"""
+
+# Exemplo 1
+print(2 == 2)
+
+# Exemplo 2
+print(2 > 2)
+
+# Exemplo 3
+print(2 >= 2)
+
+# Exemplo 4
+print(2 < 2)
+
+# Exemplo 5
+print(2 <= 2)
+
+# Exemplo 6
+print(2 != 2)
