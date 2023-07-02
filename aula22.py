ec14fddaed45300484bef0872797ed2126436f12
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-02 11:59:34 -0300

    feat: while in while

--- aula22.py
+++ aula22.py
@@ -14,11 +14,31 @@
 #     print('loop infinito')
 #     if nome == 'sair':
 #         break
-import time
+# import time
 
-contador = 0
+# contador = 0
 
-while contador < 10:
-    contador += 1
-    print(contador, end='\r')
-    time.sleep(0.5)
+# while contador < 10:
+#     contador += 1
+#     print(contador, end='\r')
+#     time.sleep(0.5)
+
+# while continue
+# while True:
+#     nome = input('Digite seu nome: ')
+#     if nome == 'sair':
+#         break
+#     if nome == 'pular':
+#         continue
+#     print(nome)
+qtd_linhas = 5
+qtd_colunas = 5
+
+# Para cada linha, imprima 5 colunas
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
