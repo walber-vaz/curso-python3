c7eaf1ee90f372079543bf09c03bb1e76fd0abf4
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-20 10:17:40 -0300

    feat: Double precision floating point number IEEE 754

---
+++ aula32.py
@@ -0,0 +1,15 @@
+"""
+Imprecisão de ponto flutuante
+Double precision floating point number IEEE 754
+"""
+num1 = 0.1
+num2 = 0.7
+sum = num1 + num2
+print("Sem formatação ->", sum)
+print("Usando format ->", f'{sum:.2f}')
+
+# usando round
+print("Usando round ->", round(sum, 2))
+
+# usando decimal
+# print("Usando decimal ->", decimal.Decimal(sum))
