0a2d4c35641ca6f3f67cfae0a46ea1d8513c112e
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-20 09:28:04 -0300

    feat: tuple

---
+++ aula31.py
@@ -0,0 +1,11 @@
+# tuplas -> E tipo de dado não pode ser alterado (mutable)
+tuplas = ("Python", "Fundamentos", "Data Science", "Web")
+
+print(tuplas)
+
+print("Imprime o primeiro elemento da tupla ->", tuplas[0])
+
+print("Imprime o ultimo elemento da tupla ->", tuplas[-1])
+
+for tupla in tuplas:
+    print(tupla)
