acd708f84af5d1881d76baf9348ad8e91bc8b9bd
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-24 10:40:32 -0300

    feat: dict in python

---
+++ aula42.py
@@ -0,0 +1,28 @@
+# Dicionario em python
+# Estrutura de dados que armazena valores de um tipo de dado associados a uma chave
+
+# Criando um dicionario
+dados: dict = {
+    "nome": "Walber",
+    "idade": 32,
+    "cidade": "São Miguel do Guamá",
+    "nacionalidade": "Brasileiro",
+    "profissao": "Programador",
+    "empresa": "Nenhuma",
+    "salario": 0.0,
+    "endereco": {
+        "rua": "Rua 1",
+        "numero": 1,
+        "bairro": "Centro",
+        "cidade": "São Miguel do Guamá",
+        "estado": "Pará",
+        "pais": "Brasil",
+    },
+}
+
+# Criando usando dict
+dados2: dict = dict(nome="Walber", idade=32, cidade="São Miguel do Guamá")
+
+# Imprimindo o dicionario
+print(dados)
+print(dados2)
