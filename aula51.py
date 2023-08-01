45b87d010fcc971ae5c788365aeac7952a93f303
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-01 10:18:17 -0300

    feat: method hasattr and getaatr

---
+++ aula51.py
@@ -0,0 +1,9 @@
+string = 'Walber'
+
+print(string)
+
+# hasattr: verifica se o objeto possui o atributo
+print(hasattr(string, '__iter__'))
+
+# getattr: retorna o valor do atributo
+print(getattr(string, '__iter__'))
